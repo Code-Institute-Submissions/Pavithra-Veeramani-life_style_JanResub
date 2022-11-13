9ab1cbe8ad395eb488471bae47bac7c6eb84f61e
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-11-13 20:29:23 +0000

    new products and categories. search and sort

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -8,10 +10,43 @@ def all_products(request):
 
     """ A view to show all products, including sorting and search queries """
 
+    categories = None
+    query = None
+    sort = None
+    order = None
+    order = None
     products = Product.objects.all()
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               ("You didn't enter any search criteria!"))
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'order' in request.GET:
+                order = request.GET['order']
+                if order == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{order}'
+            
     context = {
         'products': products,
+        'search_term': query,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'products/products.html', context)
