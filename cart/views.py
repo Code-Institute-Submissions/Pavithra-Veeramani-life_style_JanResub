7e3483d02e2a172d90c543f71582e8d1d47fa0dd
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-11-14 18:18:48 +0000

    Added shoppping cart functionality

--- cart/views.py
+++ cart/views.py
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def view_cart(request):
     """ A view that renders the bag contents page """
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.GET('redirect_url')
+    bag = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['bag'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
+
