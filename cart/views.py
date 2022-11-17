285ebc7567353ef77c0c8c01c2e275544e7918fb
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-11-17 10:36:29 +0000

    Added quantity input buttons

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -11,15 +11,15 @@ def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.GET('redirect_url')
-    bag = request.session.get('cart', {})
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
 
-    request.session['bag'] = cart
-    print(request.session['cart'])
+    request.session['cart'] = cart
+    
     return redirect(redirect_url)
 
