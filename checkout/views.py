d6149fa4f1311b135119134aab5647bb36906351
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-11-22 11:32:36 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     emplate = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M6tzPBsS8bnWf1zeIpLCLVFziZoJ19WtkQez8TIyXVwswQfnBiO62f8DKjQxXBm4MnTfka4RFxwhytbPTRCEeQe00OZFYU9Ux',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
