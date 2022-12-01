77dbca1c0b686638acf006c2f30b3db77c8f0726
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-12-01 12:05:54 +0000

    Added views in product admin

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -11,23 +12,13 @@ def all_products(request):
 
     """ A view to show all products, including sorting and search queries """
 
-    categories = None
-    query = None
-    sort = None
-    order = None
-    order = None
     products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request,
-                               ("You didn't enter any search criteria!"))
-                return redirect(reverse('products'))
-
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
@@ -41,7 +32,19 @@ def all_products(request):
                 if order == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
     current_sorting = f'{sort}_{order}'
             
     context = {
@@ -65,9 +68,13 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
-
+@login_required
 def add_product(request):
     """Add aproduct to the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -85,9 +92,13 @@ def add_product(request):
  
     return render(request, template, context)
 
-
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -109,9 +120,13 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
