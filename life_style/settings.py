01c4bcb2ab0dbe195c2d3153dce86dbd7a1a44ab
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-12-06 22:51:05 +0000

    Removed secret key and set debug

--- life_style/settings.py
+++ life_style/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gpzwy6ux3f3s4-#6ydm+n%^vcxy#=j+z$ual#l9scli++p#6x@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['life-style.heroku.com', 'localhost']
 
